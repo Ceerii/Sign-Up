d976a658168157374772eb0526018e5eeefa54e0
Chiamaka Nkem-Eze <ceerieze@gmail.com>
2019-06-21 21:44:43 +0100

    Sign Up Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+  <div className="container">
+      <form className="signUp-Form">
+      <label className="formLabel">First Name</label>
+      <input className="formInput" type="text"></input>
+      <label className="formLabel">Last Name</label>
+      <input className="formInput" type="text"></input>
+      <label className="formLabel">Email Address</label>
+      <input className="formInput" type="text"></input>
+      <label className="formLabel">Password</label>
+      <input className="formInput" type="text"></input>
+      <button className="formButton">Sign Up</button>
+      <a className="signIn-Form" href="#">Already a User?</a>
+      </form>
+   </div>
+      
+    }
     </div>
   );
 }
